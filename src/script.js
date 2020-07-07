9ddb8e1982b4dc828cda8e257699b651aedf547e
github username <github email address>
2020-07-07 19:34:06 -0400

    first commit of tracker frontend 2.0

--- src/script.js
+++ src/script.js
@@ -1,11 +1,46 @@
+const showRoutines = document.querySelector("#show-routines")
+const routineContainer = document.querySelector("#routine-container")
+const saveChanges = document.querySelector('#save-changes')
+const exerciseList = document.querySelector('#exercise-list')
+const selectButton = document.querySelector('#exercise-select')
+let exerciseArray = []
 
 
-const signIn = document.querySelector('#sign-in')
+const routines = [
+  {
+    name: "upper body",
+    exercises: ["pushups", "pullups", "situps"],
+    reps: 10,
+    sets: 3
+  },
+  {
+    name: "lower body",
+    exercises: ["squats", "lunges", "calf raises"],
+    reps: 10,
+    sets: 3 
+  }
+]
 
-signIn.addEventListener('click', e => {
-  e.preventDefault()
-  // console.log('button')
-  fetch(`http://localhost:3000/api/v1/users`)
-  .then(r => r.json())
-  .then(data => console.log(data))
+showRoutines.addEventListener('click', e => {
+  populateList(routines)
 })
+
+selectButton.addEventListener('click', e => {
+  console.log(exerciseList.value)
+})
+
+const populateList = (array) => {
+  array.forEach(item => {
+    let routineItem = document.createElement('div')
+    routineItem.className = "routine"
+    routineItem.innerHTML = `
+    <h3>${item.name}</h3>
+    <p>${item.exercises[0]} - reps: ${item.reps}</p>
+    <p>${item.exercises[1]} - reps: ${item.reps}</p>
+    <p>${item.exercises[2]} - reps: ${item.reps}</p>
+    <p>sets: ${item.sets}</p>
+    <br>
+    `
+    routineContainer.appendChild(routineItem)
+  })
+}
