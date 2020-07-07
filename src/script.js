bf5f2b41fcec0c084f6a73dc42dc5598148e104b
github username <github email address>
2020-07-07 11:59:21 -0400

    first commit

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+
+
+const signIn = document.querySelector('#sign-in')
+
+signIn.addEventListener('click', e => {
+  e.preventDefault()
+  // console.log('button')
+  fetch(`http://localhost:3000/api/v1/users`)
+  .then(r => r.json())
+  .then(data => console.log(data))
+})
