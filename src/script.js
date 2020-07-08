29c38da30d5c01092aa0d5561ccedd9a729120bb
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-07-08 16:23:14 -0400

    post and render new routine

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
+const BASE_URL = "http://localhost:3000/api/v1/"
+
 const showRoutines = document.querySelector("#show-routines")
 const routineContainer = document.querySelector("#routine-container")
 const newRoutine = document.querySelector('#new-routine')
+const modal = document.querySelector('.modal')
+const createRoutineForm = modal.querySelector(".new-routine-form")
 const exerciseList = document.querySelector('#exercise-list')
 const selectButton = document.querySelector('#exercise-select')
+const routineNameInput = document.querySelector('#create')
+
 
 
 getRoutines = callback => {
@@ -12,24 +18,6 @@ getRoutines = callback => {
     .catch(error => console.log('error', error));
 }
 
-// getRoutineExcercises = (id, callback) => {
-//   fetch(`http://localhost:3000/api/v1/exercises_for_routine/${id}`)
-//     .then(res => res.json())
-//     .then(json => callback(id, json))
-//     .catch(error => console.log('error', error));
-// }
-
-// renderRoutineExercises = (id, json) => {
-//   const card = document.querySelector(`.card-${id}`)
-//   json.forEach(exercise => {
-//     card.insertAdjacentHTML("beforeend", `
-//       <div class="card-body"> 
-//         <h5 class="card-title">${exercise.name}</h5>
-//         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-//       </div>
-//     `)
-//   })
-// }
 
 renderRoutineList = json => {
   json.forEach(item => {
@@ -74,45 +62,56 @@ renderRoutineList = json => {
 //   console.log('click')
 // })
 
-newRoutine.addEventListener('click', e => {
-  let routineNameInput = document.querySelector('#create')
-  let routineName = routineNameInput.value
+renderRoutine = routine => {
   routineNameInput.closest("form").reset()
   const newRoutineCard = document.createElement('div')
   newRoutineCard.classList = "routine"
   newRoutineCard.innerHTML = `
-  <div class="card">
-    <h3 class="card-header">${routineName}</h3>
+  <div class="card card-${routine.id}" data-routine-id="${routine.id}">
+    <h3 class="card-header">${routine.name}</h3>
       <div class="card-body">
         <form>
-             <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                      <button class="btn btn-outline-secondary" id="exercise-select" type="button">Select</button>
-                    </div>
-                    <select class="custom-select" id="exercise-list" placeholder="Exercises" aria-label="Example select with button addon">
-                      <option selected>Exercise List</option>
-                      <option value="Pushups">Pushups</option>
-                      <option value="Pullups">Pullups</option>
-                      <option value="Sit Ups">Sit Ups</option>
-                      <option value="Squats">Squats</option>
-                      <option value="Lunges">Lunges</option>
-                      <option value="Calf Raises">Calf Raises</option>
-                    </select>
-                  </div>
-                </form>
-  </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <button class="btn btn-outline-secondary" id="exercise-select" type="button">Select</button>
+            </div>
+            <select class="custom-select" id="exercise-list" placeholder="Exercises" aria-label="Example select with button addon">
+              <option selected>Exercise List</option>
+              <option value="Pushups">Pushups</option>
+              <option value="Pullups">Pullups</option>
+              <option value="Sit Ups">Sit Ups</option>
+              <option value="Squats">Squats</option>
+              <option value="Lunges">Lunges</option>
+              <option value="Calf Raises">Calf Raises</option>
+            </select>
+          </div>
+        </form>
+    </div>
   </div>
   `
   routineContainer.append(newRoutineCard)
-})
+}
 
-routineContainer.addEventListener('click', e => {
-  if (e.target.matches(".card-header")) {
-    const id = e.target.closest(".card").dataset.routineId
-    console.log(e.target.nextElementSibling);
-    
+postRoutine = (name, callback) => {
+  var requestOptions = {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({"name":`${name}`})
+  };
+  
+  fetch(`${BASE_URL}/routines`, requestOptions)
+    .then(res => res.json())
+    .then(json => callback(json))
+    .catch(error => console.log('error', error));
+}
+
+modal.addEventListener('click', e => {
+  if (e.target.matches('#new-routine')) {
+    let routineName = routineNameInput.value
+    postRoutine(routineName, renderRoutine)
   }
 })
+
 
 
 getRoutines(renderRoutineList)
